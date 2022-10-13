79fec3545949af555b49d56594a5316e7bba115b
Marco Aurélio Rehme Thomaz <marco_a_thomaz@hotmail.com>
2022-10-13 02:27:21 -0300

    Revert "Font"

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ImageBackground, StyleSheet, TouchableOpacity } from "react-native";
+import { StyleSheet, TouchableOpacity } from "react-native";
 
 import { StyledText } from "./StyledText";
 import { Text, View } from "./Themed";
@@ -8,44 +8,27 @@ import { RootStackScreenProps } from "../types";
 
 interface Props extends Partial<RootStackScreenProps<"Root">> {
   person: IPerson;
-  index: number;
 }
 
-export function Card({ person, index, navigation }: Props) {
+export function Card({ person, navigation }: Props) {
   return (
     <TouchableOpacity
       onPress={() => navigation?.navigate("Modal")}
       style={styles.container}
     >
-      <ImageBackground
-        source={{
-          uri: `https://starwars-visualguide.com/assets/img/characters/${
-            index + (index > 15 ? 2 : 1)
-          }.jpg`,
-        }}
-      >
-        <View
-          darkColor="rgba(0,0,0,0.4)"
-          lightColor="rgba(255,255,255,0.3)"
-          style={styles.container}
-        >
-          <StyledText style={styles.title}>{person.name}</StyledText>
-          <View darkColor="transparent" lightColor="transparent">
-            <Text style={styles.subtitle}>Physical Aspects:</Text>
-            <Text style={styles.numberDetails}>Height: {person.height} cm</Text>
-            <Text style={styles.numberDetails}>Mass: {person.mass} kg</Text>
-            <Text style={styles.stringDetails}>
-              Hair Color: {person.hair_color}
-            </Text>
-            <Text style={styles.stringDetails}>
-              Skin Color: {person.skin_color}
-            </Text>
-            <Text style={styles.stringDetails}>
-              Eye Color: {person.eye_color}
-            </Text>
-          </View>
-        </View>
-      </ImageBackground>
+      <StyledText style={styles.title}>{person.name}</StyledText>
+      <View>
+        <Text style={styles.subtitle}>Physical Aspects:</Text>
+        <Text style={styles.numberDetails}>Height: {person.height} cm</Text>
+        <Text style={styles.numberDetails}>Mass: {person.mass} kg</Text>
+        <Text style={styles.stringDetails}>
+          Hair Color: {person.hair_color}
+        </Text>
+        <Text style={styles.stringDetails}>
+          Skin Color: {person.skin_color}
+        </Text>
+        <Text style={styles.stringDetails}>Eye Color: {person.eye_color}</Text>
+      </View>
     </TouchableOpacity>
   );
 }
@@ -61,7 +44,6 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 20,
-    textTransform: "lowercase",
   },
   subtitle: {
     fontSize: 15,
